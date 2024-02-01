73dcb21e0ae2c36ab0e036c644894f57875c5d5b
kareem <kareemtarek015@gmail.com>
2024-02-01 22:53:17 +0200

    register adaptor

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_application_4/models/noteModel.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+
+  addNote(noteModel note) {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_4/Views/notesView.dart';
 import 'package:flutter_application_4/constants.dart';
+import 'package:flutter_application_4/models/noteModel.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(noteModelAdapter());
   runApp(const noteApp());
 }
 
+// ignore: camel_case_types
 class noteApp extends StatelessWidget {
   const noteApp({super.key});
 
