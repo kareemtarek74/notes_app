793a6f38668fec9c1dcd9bf2bd50a7d14ff9b1c9
kareem <kareemtarek015@gmail.com>
2024-02-09 03:48:07 +0200

    create fetch readNote method

--- lib/cubits/readNotes/read_notes_cubit.dart
+++ lib/cubits/readNotes/read_notes_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_application_4/models/noteModel.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+
+import '../../constants.dart';
 
 part 'read_notes_state.dart';
 
 class ReadNotesCubit extends Cubit<ReadNotesState> {
   ReadNotesCubit() : super(ReadNotesInitial());
+
+  fetchAllNotes() async {
+    emit(ReadNotesLoading());
+    try {
+      var notesBox = Hive.box<noteModel>(kNotesBox);
+      // ignore: non_constant_identifier_names
+      List<noteModel> Notes = notesBox.values.toList();
+      emit(ReadNotesSuccess(Notes));
+    } catch (e) {
+      emit(ReadNotesFailure(e.toString()));
+    }
+  }
 }
